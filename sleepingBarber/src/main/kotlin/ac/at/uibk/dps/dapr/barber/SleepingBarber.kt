01d8d3d75737618840982ae266d4e1807ad9d2a0
GregorHolzer <162716937+GregorHolzer@users.noreply.github.com>
2026-03-16 13:54:13 +0100

    feat: add dining philiosophers and sleeping barber dapr implementations (#4)
    Co-authored-by: Gregor Holzer <gregor.holzer@uibk.ac.at>

---
+++ sleepingBarber/src/main/kotlin/ac/at/uibk/dps/dapr/barber/SleepingBarber.kt
@@ -0,0 +1,51 @@
+package ac.at.uibk.dps.dapr.barber
+
+import ac.at.uibk.dps.dapr.barber.barber.BarberActorImpl
+import ac.at.uibk.dps.dapr.barber.customer.CustomerActor
+import ac.at.uibk.dps.dapr.barber.customer.CustomerActorImpl
+import ac.at.uibk.dps.dapr.barber.waitingroom.WaitingRoomActorImpl
+import io.dapr.actors.ActorId
+import io.dapr.actors.client.ActorClient
+import io.dapr.actors.client.ActorProxyBuilder
+import io.dapr.actors.runtime.ActorRuntime
+import io.dapr.client.DaprClient
+import io.dapr.client.DaprClientBuilder
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.stereotype.Component
+
+@SpringBootApplication
+class SleepingBarber {
+  companion object {
+    val logger: Logger = LoggerFactory.getLogger(SleepingBarber::class.java)
+    val daprClient: DaprClient = DaprClientBuilder().build()
+    val actorClient: ActorClient = ActorClient()
+  }
+}
+
+fun main(args: Array<String>) {
+  val role = System.getenv("ROLE") ?: "customer"
+  if (role == "waiting_room")
+    ActorRuntime.getInstance().registerActor(WaitingRoomActorImpl::class.java)
+  if (role == "barber") ActorRuntime.getInstance().registerActor(BarberActorImpl::class.java)
+  if (role == "customer") ActorRuntime.getInstance().registerActor(CustomerActorImpl::class.java)
+  runApplication<SleepingBarber>(*args)
+}
+
+@Component
+class AutoStarter : ApplicationRunner {
+
+  override fun run(args: ApplicationArguments?) {
+    val role = System.getenv("ROLE") ?: "customer"
+    if (role != "customer") return
+    val id = System.getenv("CUSTOMER_ID")
+    val proxy =
+      ActorProxyBuilder(CustomerActor::class.java, SleepingBarber.actorClient).build(ActorId(id))
+    proxy.enterWaitingRoom().subscribe()
+    SleepingBarber.logger.info("customer $id started initial")
+  }
+}
---
+++ sleepingBarber/src/main/kotlin/ac/at/uibk/dps/dapr/barber/customer/CustomerPubSub.kt
@@ -0,0 +1,66 @@
+package ac.at.uibk.dps.dapr.barber.customer
+
+import io.dapr.Topic
+import io.dapr.actors.ActorId
+import io.dapr.actors.client.ActorClient
+import io.dapr.actors.client.ActorProxyBuilder
+import io.dapr.client.DaprClient
+import io.dapr.client.domain.CloudEvent
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
+
+@RestController
+@ConditionalOnProperty("app.role", havingValue = "customer")
+class CustomerPubSub {
+
+  companion object {
+    private const val PUB_SUB_NAME = "customer_pub_sub"
+    private const val ENTER_TOPIC = "enter"
+    private const val FULL_TOPIC = "full"
+    private const val DONE_TOPIC = "done"
+
+    fun enter(client: DaprClient, id: Int): Mono<Void> {
+      return client.publishEvent(PUB_SUB_NAME, ENTER_TOPIC, id)
+    }
+
+    fun full(client: DaprClient, id: Int): Mono<Void> {
+      return client.publishEvent(PUB_SUB_NAME, FULL_TOPIC, id)
+    }
+
+    fun done(client: DaprClient, id: Int): Mono<Void> {
+      return client.publishEvent(PUB_SUB_NAME, DONE_TOPIC, id)
+    }
+  }
+
+  val id = System.getenv("CUSTOMER_ID")?.toInt() ?: 0
+
+  val customerProxy: CustomerActor? =
+    ActorProxyBuilder(CustomerActor::class.java, ActorClient()).build(ActorId(id.toString()))
+
+  @Topic(name = ENTER_TOPIC, pubsubName = PUB_SUB_NAME)
+  @PostMapping("/$ENTER_TOPIC")
+  fun enterSubscriber(@RequestBody event: CloudEvent<Int>) {
+    if (event.data == id) {
+      customerProxy!!.enterWaitingRoom().subscribe()
+    }
+  }
+
+  @Topic(name = FULL_TOPIC, pubsubName = PUB_SUB_NAME)
+  @PostMapping("/$FULL_TOPIC")
+  fun fullSubscriber(@RequestBody event: CloudEvent<Int>) {
+    if (event.data == id) {
+      customerProxy!!.waitingRoomFull().subscribe()
+    }
+  }
+
+  @Topic(name = DONE_TOPIC, pubsubName = PUB_SUB_NAME)
+  @PostMapping("/$DONE_TOPIC")
+  fun doneSubscriber(@RequestBody event: CloudEvent<Int>) {
+    if (event.data == id) {
+      customerProxy!!.doneCutting().subscribe()
+    }
+  }
+}
