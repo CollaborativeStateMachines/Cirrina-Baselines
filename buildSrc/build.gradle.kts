a763ec5784a248ea1c15f678928608bfb14e6979
Marlon Etheredge <marlon.etheredge@uibk.ac.at>
2026-03-23 14:03:48 +0100

    chore: improving project

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,6 +1,4 @@
-plugins {
-  `kotlin-dsl`
-}
+plugins { `kotlin-dsl` }
 
 repositories {
   mavenCentral()
@@ -9,8 +7,10 @@ repositories {
 
 dependencies {
   implementation("org.jetbrains.kotlin.jvm:org.jetbrains.kotlin.jvm.gradle.plugin:2.3.0")
-  implementation("org.jetbrains.kotlin.plugin.spring:org.jetbrains.kotlin.plugin.spring.gradle.plugin:2.3.0")
+  implementation(
+      "org.jetbrains.kotlin.plugin.spring:org.jetbrains.kotlin.plugin.spring.gradle.plugin:2.3.0"
+  )
   implementation("org.springframework.boot:spring-boot-gradle-plugin:3.4.4")
   implementation("io.spring.gradle:dependency-management-plugin:1.1.7")
-  implementation("com.ncorti.ktfmt.gradle:com.ncorti.ktfmt.gradle.gradle.plugin:0.25.0")
+  implementation("com.ncorti.ktfmt.gradle:com.ncorti.ktfmt.gradle.gradle.plugin:0.24.0")
 }
