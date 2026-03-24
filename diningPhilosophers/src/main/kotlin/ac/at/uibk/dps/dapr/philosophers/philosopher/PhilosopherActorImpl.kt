7b6ee09471fb05045c7dcaafdd2b33baea51ba82
AlexZangerle <alex.z12angerle1@gmail.com>
2026-03-24 11:41:08 +0100

    style: fixing formatting style

--- diningPhilosophers/src/main/kotlin/ac/at/uibk/dps/dapr/philosophers/philosopher/PhilosopherActorImpl.kt
+++ diningPhilosophers/src/main/kotlin/ac/at/uibk/dps/dapr/philosophers/philosopher/PhilosopherActorImpl.kt
@@ -5,11 +5,11 @@ import ac.at.uibk.dps.dapr.philosophers.arbitrator.ArbitratorPubSub
 import io.dapr.actors.ActorId
 import io.dapr.actors.runtime.AbstractActor
 import io.dapr.actors.runtime.ActorRuntimeContext
-import reactor.core.publisher.Mono
 import java.time.Duration
 import java.util.concurrent.TimeUnit
 import kotlin.time.measureTime
 import kotlin.time.toJavaDuration
+import reactor.core.publisher.Mono
 
 class PhilosopherActorImpl(runtimeContext: ActorRuntimeContext<PhilosopherActorImpl>, id: ActorId) :
   AbstractActor(runtimeContext, id), PhilosopherActor {
