640f32fe7d2790b258a56907958ffe8b1ce1947a
AlexZangerle <44397648+AlexZangerle@users.noreply.github.com>
2026-03-26 17:28:22 +0100

    fix: aligning count example with csm version (#30)

--- count/src/main/kotlin/ac/at/uibk/dps/dapr/count/producer/ProducerActorImpl.kt
+++ count/src/main/kotlin/ac/at/uibk/dps/dapr/count/producer/ProducerActorImpl.kt
@@ -1,33 +1,38 @@
 package ac.at.uibk.dps.dapr.count.producer
 
-import ac.at.uibk.dps.dapr.count.Count
 import io.dapr.actors.ActorId
 import io.dapr.actors.runtime.AbstractActor
 import io.dapr.actors.runtime.ActorRuntimeContext
 import io.dapr.client.DaprClient
 import io.dapr.client.DaprClientBuilder
+import java.security.SecureRandom
+import kotlin.random.Random
 import kotlin.time.Clock
-import kotlin.time.measureTime
-import kotlin.time.toJavaDuration
 
 class ProducerActorImpl(runtimeContext: ActorRuntimeContext<ProducerActorImpl>, actorId: ActorId) :
   AbstractActor(runtimeContext, actorId), ProducerActor {
   val client: DaprClient = DaprClientBuilder().build()
 
-  var metricRegistry = Count.provideMetricRegistry()
-  var produceTimer = metricRegistry.timer("produce.duration")
+  private val seedGenerator = SecureRandom()
+  private val threadRng =
+    object : ThreadLocal<Random>() {
+      override fun initialValue(): Random {
+        return Random(seedGenerator.nextLong())
+      }
+    }
 
   override fun produce() {
     while (true) {
-      val delta = measureTime {
-        val now = Clock.System.now()
-        val epochNanos = (now.epochSeconds * 1_000_000_000L) + now.nanosecondsOfSecond
+      Thread.sleep(randomAround(10, 2).toLong())
 
-        client.publishEvent("pubsub", "increment", epochNanos).subscribe()
+      val now = Clock.System.now()
+      val epochNanos = (now.epochSeconds * 1_000_000_000L) + now.nanosecondsOfSecond
 
-        Thread.sleep(10)
-      }
-      produceTimer.update(delta.toJavaDuration())
+      client.publishEvent("pubsub", "increment", epochNanos).subscribe()
     }
+  }
+
+  private fun randomAround(base: Int, delta: Int): Int {
+    return (base - delta..base + delta).random(threadRng.get())
   }
 }
