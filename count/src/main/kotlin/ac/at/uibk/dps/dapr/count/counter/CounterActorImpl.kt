1e1db981e69c8e4862efc502a39ba0038b63e61c
AlexZangerle <alex.z12angerle1@gmail.com>
2026-03-26 17:24:10 +0100

    fix: aligning count example with csm version

--- count/src/main/kotlin/ac/at/uibk/dps/dapr/count/counter/CounterActorImpl.kt
+++ count/src/main/kotlin/ac/at/uibk/dps/dapr/count/counter/CounterActorImpl.kt
@@ -6,8 +6,6 @@ import io.dapr.actors.runtime.AbstractActor
 import io.dapr.actors.runtime.ActorRuntimeContext
 import java.util.concurrent.TimeUnit
 import kotlin.time.Clock
-import kotlin.time.measureTime
-import kotlin.time.toJavaDuration
 
 class CounterActorImpl(runtimeContext: ActorRuntimeContext<CounterActorImpl>, actorId: ActorId) :
   AbstractActor(runtimeContext, actorId), CounterActor {
@@ -15,22 +13,15 @@ class CounterActorImpl(runtimeContext: ActorRuntimeContext<CounterActorImpl>, ac
   var count = 0
 
   var metricRegistry = Count.provideMetricRegistry()
-  var counter = metricRegistry.counter("counter.count")
-  var eventTimer = metricRegistry.timer("event.latency")
-  var incrementTimer = metricRegistry.timer("increment.duration")
 
   override fun increment(time: Long) {
-    val delta = measureTime {
-      val now = Clock.System.now()
-      val nowNanos = (now.epochSeconds * 1_000_000_000L) + now.nanosecondsOfSecond
+    val now = Clock.System.now()
+    val nowNanos = (now.epochSeconds * 1_000_000_000L) + now.nanosecondsOfSecond
+    val deltaNanos = (nowNanos - time).coerceAtLeast(0L)
 
-      val deltaNanos = (nowNanos - time).coerceAtLeast(0L)
+    metricRegistry.timer("event.latency").update((deltaNanos), TimeUnit.NANOSECONDS)
 
-      eventTimer.update((deltaNanos), TimeUnit.NANOSECONDS)
-
-      count += 1
-      counter.inc()
-    }
-    incrementTimer.update(delta.toJavaDuration())
+    ++count
+    metricRegistry.counter("counter.count").inc()
   }
 }
