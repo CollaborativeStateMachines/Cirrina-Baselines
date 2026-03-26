91700ba12cbf0fcfccafeb1ba49d3493056e03b4
AlexZangerle <alex.z12angerle1@gmail.com>
2026-03-26 21:04:48 +0100

    fix: aligning big with csm version

--- big/src/main/kotlin/ac/at/uibk/dps/dapr/big/big/BigSubscriber.kt
+++ big/src/main/kotlin/ac/at/uibk/dps/dapr/big/big/BigSubscriber.kt
@@ -4,6 +4,7 @@ import io.dapr.Topic
 import io.dapr.actors.ActorId
 import io.dapr.actors.client.ActorClient
 import io.dapr.actors.client.ActorProxyBuilder
+import io.dapr.client.domain.CloudEvent
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -17,33 +18,25 @@ class BigSubscriber {
   private val bigProxy =
     ActorProxyBuilder(BigActor::class.java, ActorClient()).build(ActorId("$id"))
 
-  @Topic(name = "neighbors", pubsubName = "pubsub")
-  @PostMapping("/neighbors")
-  fun handleNeighbors(@RequestBody body: Map<String, Any>) {
-    val neighbors = body["data"] as List<String>
-
-    bigProxy.assignNeighbors(neighbors)
+  @Topic(name = "initial", pubsubName = "pubsub")
+  @PostMapping("/initial")
+  fun initialSubscriber() {
+    bigProxy.initial()
   }
 
   @Topic(name = "ping", pubsubName = "pubsub")
   @PostMapping("/ping")
-  fun handlePing(@RequestBody body: Map<String, Any>) {
-    val data = body["data"] as? Map<String, Any> ?: body
-    val receiver = data["receiver"] as? String ?: return
-
-    if (receiver == id) {
-      bigProxy.sendPong(data)
+  fun pingSubscriber(@RequestBody event: CloudEvent<Map<String, Any>>) {
+    if (event.data["target"].toString() == id) {
+      bigProxy.onPing(event.data)
     }
   }
 
   @Topic(name = "pong", pubsubName = "pubsub")
   @PostMapping("/pong")
-  fun handlePong(@RequestBody body: Map<String, Any>) {
-    val data = body["data"] as? Map<String, Any> ?: body
-    val receiver = data["receiver"] as? String ?: return
-
-    if (receiver == id) {
-      bigProxy.receivePong(data)
+  fun pongSubscriber(@RequestBody event: CloudEvent<Map<String, Any>>) {
+    if (event.data["target"] == id) {
+      bigProxy.onPong(event.data)
     }
   }
 }
